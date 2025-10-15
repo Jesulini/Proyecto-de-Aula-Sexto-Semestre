5d63905a4951a98e1041b6fa3bd255a8b59b6e4c
Ishigaming <juanmar31500@gmail.com>
2025-10-14 22:31:10 -0500

    Standalone: false puestos xd

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Cinefilia',
+  webDir: 'www'
+};
+
+export default config;
