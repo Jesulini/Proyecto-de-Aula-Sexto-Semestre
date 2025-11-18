962f2fb2c6dc9e5a99aef4bbc80c5268d70e4859
JericothCastellar <jericothcastellar06@gmail.com>
2025-11-17 20:19:16 -0500

    Se añadio el metodo de pago simulado, se hizo una firebase para este

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+   production: true,
   firebaseConfig: {
     apiKey: "AIzaSyD9PRzg4o0sErdbV_oMQW8Mi2jRrgrWYkk",
     authDomain: "cinephilia-dbe85.firebaseapp.com",
@@ -8,6 +8,14 @@ export const environment = {
     messagingSenderId: "207738614681",
     appId: "1:207738614681:web:9eb6f068bfcb49fdd01908",
     measurementId: "G-B1JXSFH5XN"
+  },
+  firebaseSubscriptionsConfig: {
+    apiKey: "AIzaSyBGQIUVqaYDpqNL47IAiTLqtWB8F6pSsxE",
+    authDomain: "movieapp-subscriptions-12345.firebaseapp.com",
+    projectId: "movieapp-subscriptions-12345",
+    storageBucket: "movieapp-subscriptions-12345.firebasestorage.app",
+    messagingSenderId: "213445224554",
+    appId: "1:213445224554:web:4a1eaa02d5e7444d6b4213"
   },
   supabaseUrl: 'https://wpvwredryjdspdnmpvfb.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwdndyZWRyeWpkc3Bkbm1wdmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NzY0NDcsImV4cCI6MjA3ODM1MjQ0N30.d73irZMpG-lpzu2u2oDul8IBVWyoGCfSahYkOe8BzXw',
